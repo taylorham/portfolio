513759da32a64f2d130f8e8139325f5e379111a3
Taylor Ham <mynameisham@gmail.com>
2016-01-16 15:41:07 -0800

    adds components for each page element

---
+++ app/components/gravatar-pic/component.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  // didInitAttrs() {
+  //   console.log('>>>>> foo');
+  //   this.attrs.sha = myShaFunc(this.attrs.email);
+  //   this.attrs.sha = 'testies';
+  // }
+});
